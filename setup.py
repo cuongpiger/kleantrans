f71c0c3ee5e2513ad89fc72b415f5d16b4102750
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-09-03 18:28:25 +0700

    [feat] release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kleantrans",
-    version="0.0.10",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,  # Include data files specified in MANIFEST.in
     package_data={
