2354dc3cac7167dfdc2fdfcedfabcb190d31b71a
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-09-03 20:47:03 +0700

    [chore] nothing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kleantrans",
+    version="1.0.4",
+    packages=find_packages(),
+    include_package_data=True,  # Include data files specified in MANIFEST.in
+    package_data={
+        '': ['app/images/*'],  # Include all files from the images folder
+    },
+)
